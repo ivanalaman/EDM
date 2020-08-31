0162ab94444be91a702014319b8cf3c52a28ac6e
ivanalaman@gmail.com <ivanalaman@gmail.com>
2020-08-31 19:48:54 -0300

    2020.08.19_19:48hs I.B.Allaman contributions to new version

--- inst/shiny/app/server_update_app.r
+++ inst/shiny/app/server_update_app.r
@@ -2,7 +2,7 @@
 
 observeEvent(input$upp_app,{
 
-versionnew <- reactive({
+ versionnew <- reactive({
   repos <- readLines("https://github.com/ivanalaman/EDM/blob/master/DESCRIPTION")
   posver <- grep("Version",repos)
   versionsujo <- repos[posver]
@@ -26,39 +26,46 @@ versionnew <- reactive({
   #Dispara um pop-up dizendo a versão atual e a versão disponível e perguntando se gostaria de atualizar a atual versão
 
   showModal(modalDialog(
-   title = "",
-   uiOutput("matualiapp"),
-   footer = tagList(
-    modalButton(uiOutput("bnao")),
-    actionButton("ok",uiOutput("bsim"))
-   )
-  ))
+    title = "",
+    uiOutput("matualiapp"),
+    footer = tagList(
+     modalButton(uiOutput("bnao")),
+     actionButton("ok",uiOutput("bsim"))
+    )
+    ))
  }
 })
 
 observeEvent(input$ok, {
- #Agora começa-se o procedimento de atualização
- pathlibraries <- .libPaths()
- setwd(pathlibraries)#Este comando foi necessário, pois ao atualizar o app removendo o pacote, o diretório fica inexistente, gerando um erro do tiop getwcd() failed: No such file or directory. 
 
- #filetemp <- tempfile()
- #download.file(url = "https://github.com/ivanalaman/EDM/archive/master.zip",destfile = filetemp)
- #unzip(filetemp,exdir=tempdir())
- #unlink(paste0(tempdir(),"/EDM-master/inst/questionbank"),recursive=TRUE)
+ withProgress(message = tr("mgbase"), min=1,max=30,{
+  Sys.sleep(1)
+  #Agora começa-se o procedimento de atualização
+  pathlibraries <- .libPaths()
+  setwd(pathlibraries)#Este comando foi necessário, pois ao atualizar o app removendo o pacote, o diretório fica inexistente, gerando um erro do tiop getwcd() failed: No such file or directory. 
 
- pathEDM <- find.package("EDM")#encontrando o diretório do EDM
- pathqbank <- paste0(pathEDM,"/questionbank")#fazendo o caminho do banco de questões
- copysecure <- file.copy(from=pathqbank,to=tempdir(),recursive=TRUE)#copiando o banco de questões do usuário para um local seguro
+  #filetemp <- tempfile()
+  #download.file(url = "https://github.com/ivanalaman/EDM/archive/master.zip",destfile = filetemp)
+  #unzip(filetemp,exdir=tempdir())
+  #unlink(paste0(tempdir(),"/EDM-master/inst/questionbank"),recursive=TRUE)
 
- remove.packages("EDM") #removendo o pacote EDM desatualizado!
- devtools::install_github("ivanalaman/EDM") #instalando nova versão!
- file.copy(from=paste0(tempdir(),"/questionbank"),to=pathEDM,recursive=TRUE)#copiando o banco de questões do usuário de volta para o pacote
+  pathEDM <- find.package("EDM")#encontrando o diretório do EDM
+  pathqbank <- paste0(pathEDM,"/questionbank")#fazendo o caminho do banco de questões
+  pathwidgets <- paste0(pathEDM,"/aux_files/widgets")#fazendo o caminho para os widgets com o intuito de garantir que o banco de questões do usuário seja reconhecido após a atualização
+  copysecure <- file.copy(from=pathqbank,to=tempdir(),recursive=TRUE)#copiando o banco de questões do usuário para um local seguro
+  copysecurewid <- file.copy(from=pathwidgets,to=tempdir(),recursive=TRUE)#copiando o widgets antigo do usuário para um local seguro 
 
- stopApp()
- showModal(modalDialog(
-   title = "",
-   uiOutput("mreiniciar2"),
-   footer = modalButton("OK!"),
-   size='m'))
+  remove.packages("EDM") #removendo o pacote EDM desatualizado!
+  devtools::install_github("ivanalaman/EDM") #instalando nova versão!
+  file.copy(from=paste0(tempdir(),"/questionbank"),to=pathEDM,recursive=TRUE)#copiando o banco de questões do usuário de volta para o pacote
+  file.copy(from=paste0(tempdir(),"/aux_files"),to=pathEDM,recursive=TRUE)#copiando o widgets do usuário de volta para o pacote
+
+  stopApp()
+  showModal(modalDialog(
+    title = "",
+    tr("mreiniciar"),
+    footer = modalButton("OK!"),
+    size='m'))
+ })
 })
 
